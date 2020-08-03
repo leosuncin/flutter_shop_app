d048be4472c0e3a1bfc985a2880b765bbdcd7448
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-08-03 14:56:35 -0600

    Style product item
    Change theme.
    Add custom fonts.

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -9,24 +9,31 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
+    final theme = Theme.of(context);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: theme.accentColor,
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            color: theme.accentColor,
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
         ),
       ),
     );
